7f2cf625d47442a69d42e47268bec849726a908b
ilgenio <krlosfuentes@gmail.com>
2021-09-15 18:10:29 +0200

    exercise3 sample the texture

--- Exercise3.cpp
+++ Exercise3.cpp
@@ -14,14 +14,15 @@ bool Exercise3::init()
     struct Vertex
     {
         XMFLOAT3 position;
+        XMFLOAT2 uv;
     };
 
     static Vertex vertices[4] = 
     {
-        XMFLOAT3(-1.0f, -1.0f, 0.0f),  // 0
-        XMFLOAT3(-1.0f, 1.0f, 0.0f),   // 1
-        XMFLOAT3(1.0f, 1.0f, 0.0f),    // 2
-        XMFLOAT3(1.0f, -1.0f, 0.0f)    // 3
+        { XMFLOAT3(-1.0f, -1.0f, 0.0f),  XMFLOAT2(0.0f, 1.0f) },
+        { XMFLOAT3(-1.0f, 1.0f, 0.0f),   XMFLOAT2(0.0f, 0.0f) },
+        { XMFLOAT3(1.0f, 1.0f, 0.0f),    XMFLOAT2(1.0f, 0.0f) },
+        { XMFLOAT3(1.0f, -1.0f, 0.0f),   XMFLOAT2(1.0f, 1.0f) }
     };
 
     static short indices[6] = 
@@ -259,7 +260,8 @@ bool Exercise3::createRootSignature()
 
 bool Exercise3::createPSO()
 {
-    D3D12_INPUT_ELEMENT_DESC inputLayout[] = {{"POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0}};
+    D3D12_INPUT_ELEMENT_DESC inputLayout[] = {{"POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0},
+                                              {"TEXCOORD", 0, DXGI_FORMAT_R32G32_FLOAT, 0, D3D12_APPEND_ALIGNED_ELEMENT, D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0}};
 
     D3D12_INPUT_LAYOUT_DESC inputLayoutDesc = {};
     inputLayoutDesc.NumElements = sizeof(inputLayout) / sizeof(D3D12_INPUT_ELEMENT_DESC);
--- Game/Shaders/exercise3.hlsl
+++ Game/Shaders/exercise3.hlsl
@@ -4,15 +4,31 @@ cbuffer Transforms : register(b0)
     float4x4 mvp;
 };
 
+struct VertexInput
+{
+    float3 position : POSITION;
+    float2 texCoord : TEXCOORD;
+};
+
+struct VertexOutput
+{
+    float4 position : SV_POSITION;
+    float2 texCoord : TEXCOORD;
+};
+
+VertexOutput exercise3VS(VertexInput input) 
+{
+    VertexOutput output;
+    output.position = mul(float4(input.position, 1.0f), mvp);
+    output.texCoord = input.texCoord;
+
+    return output;
+}
+
 Texture2D t1 : register(t0);
 SamplerState s1 : register(s0);
 
-float4 exercise3VS(float3 pos : POSITION) : SV_POSITION
+float4 exercise3PS(VertexOutput input) : SV_TARGET
 {
-    return mul(float4(pos, 1.0f), mvp);
-}
-
-float4 exercise3PS() : SV_TARGET
-{
-    return t1.Sample(s1, float2(0.0, 0.0));
+    return t1.Sample(s1, input.texCoord);
 }
