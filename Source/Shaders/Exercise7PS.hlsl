de5d7515125a6d3360533e79d57e53aee01c98a2
ilgenio <krlosfuentes@gmail.com>
2025-05-30 15:41:03 +0200

    Some fixes

--- Source/Shaders/Exercise7PS.hlsl
+++ Source/Shaders/Exercise7PS.hlsl
@@ -5,12 +5,13 @@ SamplerState diffuseSamp : register(s0);
 
 float3 shlick(float3 rf0, float dotNL)
 {
-    return rf0 + (1 - rf0) * pow(dotNL, 5);
+    return rf0 + (1 - rf0) * pow(1.0-dotNL, 5);
 }
 
 float4 exercise7PS(float3 worldPos : POSITION, float3 normal : NORMAL, float2 coord : TEXCOORD) : SV_TARGET
 {
     float3 Cd       = hasDiffuseTex ? diffuseTex.Sample(diffuseSamp, coord).rgb * diffuseColour.rgb : diffuseColour.rgb;
+        
     float3 N        = normalize(normal);
     float3 R        = reflect(L, N);
     float3 V        = normalize(viewPos - worldPos);
@@ -19,8 +20,8 @@ float4 exercise7PS(float3 worldPos : POSITION, float3 normal : NORMAL, float2 co
     
     float rf0Max   = max(max(specularColour.r, specularColour.g), specularColour.b);
     float3 fresnel = shlick(specularColour, dotNL);
-    float3 colour  = ((Cd * (1.0 - rf0Max)) / PI + ((shininess + 2.0) / (2 * PI)) * fresnel * pow(dotVR, shininess)) * Lc * dotNL;
-
-    return float4(colour, 1.0);
+    float3 colour  = ((Cd * (1.0 - rf0Max))  + ((shininess + 2.0) / (2 )) * fresnel * pow(dotVR, shininess)) * Lc * dotNL + Ac*Cd;
+    
+    return float4(colour, 1.0); 
 }
 
