01048977ce8a2b48cf607bc85388588b2cb78f99
Carlos Fuentes de los Santos <krlosfuentes@gmail.com>
2026-06-20 11:31:27 +0200

    colour rejection

--- Source/Shaders/taaCS.hlsl
+++ Source/Shaders/taaCS.hlsl
@@ -61,6 +61,32 @@ void main(uint2 globalIdx : SV_DispatchThreadID)
     float3 prevColor = taaResult[prevPos].rgb;    
     float3 currentColor = current[globalIdx].rgb;
 
+    // colour clampling
+
+    float3 minColor = 9999.0;
+    float3 maxColor = -9999.0;
+
+    for(int i = -1; i <= 1; ++i)
+    {
+        for(int j = -1; j <= 1; ++j)
+        {
+            uint2 offsetPos = prevPos + uint2(i, j);
+
+            if(offsetPos.x < 0 || offsetPos.x >= prevWidth || offsetPos.y < 0 || offsetPos.y >= prevHeight)
+            {
+                continue;
+            }
+            float3 sampleColor = current[offsetPos].rgb;
+
+            minColor = min(minColor, sampleColor);
+            maxColor = max(maxColor, sampleColor);
+        }
+    }
+
+    prevColor = clamp(prevColor, minColor, maxColor);
+
+    // TODO: Depth rejection 
+
     // Blending factor, can be adjusted for more or less smoothing
     const float alpha = 0.1; 
 
